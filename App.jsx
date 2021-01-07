1b646ec10da84eed644a6ac872dfe431109fb982
scanner0928 <regist0928@gmail.com>
2021-01-07 17:02:59 +0900

    hello.jsx

--- App.jsx
+++ App.jsx
@@ -2,9 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello
+        style={{
+          fontSize: 16,
+        }}
+      >
+        HELL WORLD
+      </Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style='auto' />
