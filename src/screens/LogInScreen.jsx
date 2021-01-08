3b2d09ca1078aa46e91223b8ea44ca5b606bec11
scanner0928 <regist0928@gmail.com>
2021-01-08 16:52:50 +0900

    coding finished

---
+++ src/screens/LogInScreen.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+
+import AppBar from '../components/AppBar';
+import Button from '../components/Button';
+
+export default function LogInScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput
+          value="Email address"
+          style={styles.input}
+        />
+        <TextInput value="password" style={styles.input} />
+        <Button label="Submit" />
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            Not registerd?
+          </Text>
+          <Text style={styles.footerLink}>
+            Sign up here!
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 25,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    backgroundColor: '#ffffff',
+    borderColor: '#dddddd',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467fd3',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+});
